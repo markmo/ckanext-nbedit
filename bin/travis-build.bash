d9b9eaab411d2df903b8367e606426d54eb9af1f
Mark Moloney <m4rkmo@gmail.com>
2020-01-20 22:34:27 +1100

    Remove deprecated 'allow-all-external' pip install option from travis build script, change travis build script to use CKAN branch 2.8.

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -10,12 +10,14 @@ sudo apt-get install solr-jetty
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
-echo "CKAN branch: $latest_ckan_release_branch"
-git checkout $latest_ckan_release_branch
+# export latest_ckan_release_branch=`git branch --all | grep remotes/origin/release-v | sort -r | sed 's/remotes\/origin\///g' | head -n 1`
+# echo "CKAN branch: $latest_ckan_release_branch"
+# git checkout $latest_ckan_release_branch
+echo "CKAN branch: 2.8"
+git checkout 2.8
 python setup.py develop
-pip install -r requirements.txt --allow-all-external
-pip install -r dev-requirements.txt --allow-all-external
+pip install -r requirements.txt
+pip install -r dev-requirements.txt
 cd -
 
 echo "Creating the PostgreSQL user and database..."
